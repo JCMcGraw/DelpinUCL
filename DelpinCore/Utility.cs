db315785adf5e6533e42f32043efb436089bcae4
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2019-05-14 19:17:25 +0200

    Added Utility class

---
+++ DelpinCore/Utility.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DelpinCore
+{
+    static class Utility
+    {
+        public static string connectionString
+        {
+            get
+            {
+                SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+                builder.DataSource = "den1.mssql8.gear.host";
+                builder.UserID = "delpinucl";
+                builder.Password = "delpin!";
+                builder.InitialCatalog = "delpinucl";
+
+                return builder.ConnectionString;
+            }
+            private set
+            {
+                connectionString = value;
+            }
+        }
+    }
+}
