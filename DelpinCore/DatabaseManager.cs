587daff1ba41c57e011d9591d36852ea38417a36
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2019-05-14 19:46:51 +0200

    Added DatabaseManager

---
+++ DelpinCore/DatabaseManager.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace DelpinCore
+{
+    static class DatabaseManager
+    {
+        static public string CreateUpdateDeleteInDatabase(string SQL)
+        {
+            try
+            { 
+                //initialize new SqlConnection with connectionstring from Utility Class
+                using (SqlConnection connection = new SqlConnection(Utility.connectionString))
+                {
+                    //open connection
+                    connection.Open();
+
+                    //initialize SqlCommand
+                    using (SqlCommand sqlCommand = new SqlCommand(SQL, connection))
+                    {
+                        //execute sql to server
+                        sqlCommand.ExecuteNonQuery();
+                    }
+                }
+                //return success if query executed properly
+                return "success";
+            }
+            catch (Exception e)
+            {
+                //return error message if a problem arose
+                return e.Message;
+            }
+        }
+
+        static public DataTable ReadFromDatabase(string SQL)
+        {
+            try
+            {
+                //initialize new DataTable
+                DataTable dataTable = new DataTable();
+
+                //initialize new SqlConnection with connectionstring from Utility Class
+                using (SqlConnection connection = new SqlConnection(Utility.connectionString))
+                {
+                    //open connection
+                    connection.Open();
+
+                    //initialize SqlCommand
+                    using (SqlCommand command = new SqlCommand(SQL, connection))
+                    {
+                        //execute sql to server and fill dataTable with returned result
+                        SqlDataAdapter sqlDataAdapter = new SqlDataAdapter(command);
+                        sqlDataAdapter.Fill(dataTable);
+                    }
+                }
+
+                //return dataTable
+                return dataTable;
+            }
+            catch (Exception e)
+            {
+                //return empty DataTable if problem arose
+                return new DataTable();
+            }
+        }
+    }
+}
