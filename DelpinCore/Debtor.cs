07e6777cae5f2a3c7e9c9d88b405e3ba9ded2fee
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-05-15 09:46:17 +0200

    Personal Class

--- DelpinCore/Debtor.cs
+++ DelpinCore/Debtor.cs
@@ -33,6 +33,6 @@ namespace DelpinCore
             this.email = email;
         }
 
-        //hej
+        //hejse
     }
 }
---
+++ DelpinCore/Personal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelpinCore
+{
+    class Personal : Debtor
+    {
+        public int CPR { get; private set; }
+
+        public Personal(int debtorID, string name, string adress, int postalCode, string city, string phone, string email) : base(debtorID, name, adress, postalCode, city, phone, email)
+        {
+
+        }
+
+    }
+}
