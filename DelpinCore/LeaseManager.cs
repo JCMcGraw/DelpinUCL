d9b5548fda0c3fe43101f93cc42626309730d64c
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2019-05-15 11:43:02 +0200

    Method to delete an existing Lease

--- DelpinCore/LeaseManager.cs
+++ DelpinCore/LeaseManager.cs
@@ -10,21 +10,50 @@ namespace DelpinCore
     {
         private Lease activeLease;
 
+        //dummy method, doesn't do anything yet
         public void ReadLease(int leaseID)
         {
 
         }
 
-        public void DeleteLease(int leaseID)
+        //Method to delete a Lease
+        public string DeleteLease(int leaseID)
         {
+            //sql to delete all LeaseOrders from the Lease
+            string deleteLeaseOrders = $"delete from LeaseOrder where LeaseID = {leaseID}";
 
+            //sql to delete the Lease
+            string deleteLease = $"delete from Lease where LeaseID = {leaseID}";
+
+            //send sql to delete LeaseOrders
+            string isDeleteLeaseOrderSuccess = DatabaseManager.CreateUpdateDeleteInDatabase(deleteLeaseOrders);
+
+            //if deletion of LeaseOrders was not a success return the error message
+            if (isDeleteLeaseOrderSuccess != "success")
+            {
+                return isDeleteLeaseOrderSuccess;
+            }
+
+            //send sql to delete Lease
+            string isDeleteLeaseSuccess = DatabaseManager.CreateUpdateDeleteInDatabase(deleteLease);
+            
+            //if deletion of LeaseOrders was not a success return the error message
+            if (isDeleteLeaseSuccess != "success")
+            {
+                return isDeleteLeaseSuccess;
+            }
+
+            //if everything went right, return message that Lease was deleted
+            return $"Lejekontrakt {leaseID} er blevet slettet";
         }
 
+        //dummy method, doesn't do anything yet
         public void CreateLease(int debtorID)
         {
             activeLease = new Lease(debtorID);
         }
 
+        //dummy method, doesn't do anything yet
         public void AddLeaseOrderToLease(LeaseOrder leaseOrder)
         {
             activeLease.AddLeaseOrder(leaseOrder);
