ed760dc247afc0ddef7e75d74a7b49821ca45699
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-05-15 14:29:48 +0200

    More methods in Debtormanager

--- DelpinCore/DebtorManager.cs
+++ DelpinCore/DebtorManager.cs
@@ -53,5 +53,54 @@ namespace DelpinCore
 
             return DatabaseManager.CreateUpdateDeleteInDatabase(SQL);
         }
+
+        public string CreateBusinessDebtor(int debtorID, string name, string adress, int postalCode, string city, string phone, string email, int CVR)
+        {
+            string SQL;
+
+            SQL = "Insert into Debtor(debtorID, name, adress,postalCode,city,phone,email) " +
+                  $"values ({debtorID},'{name}','{adress}','{postalCode}','{city}','{phone}','{email})" +
+
+                  "Insert into Business(debtorID, name, adress,postalCode,city,phone,email,CVR) " +
+                  $"values ({debtorID},'{name}','{adress}','{postalCode}','{city}','{phone}','{email}','{CVR})";
+
+            return DatabaseManager.CreateUpdateDeleteInDatabase(SQL);
+        }
+
+        public string ReadBusinessDebtor()
+        {
+            string SQL;
+
+            SQL = "Select * from Debtor" +
+                  "Select * from Business";
+
+            return Convert.ToString(DatabaseManager.ReadFromDatabase(SQL));
+        }
+
+        public string UpdateBusinessDebtor(int debtorID, string name, string adress, int postalCode, string city, string phone, string email, int CVR)
+        {
+            string SQL;
+
+            SQL = $"update Debtor set debtorID={debtorID},name='{name}',adress='{adress}" +
+                $",postalCode='{postalCode},city='{city},phone='{phone},email='{email}" +
+
+                $"update Business set debtorID={debtorID},name='{name}',adress='{adress}" +
+                $",postalCode='{postalCode},city='{city},phone='{phone},email='{email},CPR='{CVR}";
+
+            return DatabaseManager.CreateUpdateDeleteInDatabase(SQL);
+        }
+
+        public string DeleteBusinessDebtor(int debtorID)
+        {
+            string SQL;
+
+            SQL = $"Delete from Debtor where debtorID={debtorID}" +
+                  $"Delete from Business where debtorID={debtorID}";
+
+            return DatabaseManager.CreateUpdateDeleteInDatabase(SQL);
+        }
+
+
+
     }
 }
