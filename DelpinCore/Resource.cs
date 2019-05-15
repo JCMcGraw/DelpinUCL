3569023ee6b9633ea6c855be5abe70eb056fe2d0
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-05-15 10:25:14 +0200

    Made constructor more to resource

--- DelpinCore/Resource.cs
+++ DelpinCore/Resource.cs
@@ -14,11 +14,38 @@ namespace DelpinCore
 
         public double listPrice { get; private set; }
 
+        List<Resource> accesories = new List<Resource>();
+
+        public Resource(int resourceID)
+        {
+            this.resourceID = resourceID;
+        }
+
         public Resource(int resourceID, string modelName, double listPrice)
         {
             this.resourceID = resourceID;
             this.modelName = modelName;
             this.listPrice = listPrice;
+        }
+
+        public void AddResource(Resource resource)
+        {
+            accesories.Add(resource);
+        }
+
+        public void AddResourceAtSpecificIndes(Resource resource, int indexToAddAt)
+        {
+            accesories.Insert(indexToAddAt, resource);
+        }
+
+        public void RemoveResource(int indexToRemove)
+        {
+            accesories.RemoveAt(indexToRemove);
+        }
+
+        public List<Resource> GetResource()
+        {
+            return accesories;
         }
     }
 }
--- DelpinCore/ResourceManager.cs
+++ DelpinCore/ResourceManager.cs
@@ -8,5 +8,27 @@ namespace DelpinCore
 {
     class ResourceManager
     {
+        private Resource activeResource;
+
+        public void ReadResource(int resourceID)
+        {
+            //activeResource.AddResource(resourceID);
+        }
+
+        public void DeleteResource(int resourceID)
+        {
+            //activeResource.AddResource(resourceID);
+        }
+
+        public void CreateResource(int debtorID)
+        {
+            //activeResource = new Resource(debtorID);
+        }
+
+        public void AddResourceToResource(Resource accesories)
+        {
+            activeResource.AddResource(accesories);
+        }
+
     }
 }
