16f9128ee3c177c46e897ad24ac479e273b1b4e2
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-05-20 10:02:40 +0200

    changes

--- DelpinCore/Resource.cs
+++ DelpinCore/Resource.cs
@@ -10,9 +10,15 @@ namespace DelpinCore
     {
         public int resourceID { get; private set; }
 
-        public string modelID { get; private set; }
+        public int modelID { get; private set; }
 
-        public double branchID { get; private set; }
+        public int branchID { get; private set; }
+
+        public string modelName { get; private set; }
+
+        public double price { get; private set; }
+
+        public int subGroupID { get; private set; }
 
         //Constructor
         public Resource(int resourceID)
@@ -21,11 +27,14 @@ namespace DelpinCore
         }
 
         //Constructor
-        public Resource(int resourceID, string modelID, int branchID)
+        public Resource(int resourceID, int modelID, int branchID,string modelName,double price,int subGroupID)
         {
             this.resourceID = resourceID;
             this.modelID = modelID;
             this.branchID = branchID;
+            this.modelName = modelName;
+            this.price = price;
+            this.subGroupID = subGroupID;
         }
     }
 }
