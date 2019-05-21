3f94f32d5f1c1780a2f3fe6a16d91f01df641579
Daniel Raffnsøe Dall <dani8941@edu.eal.dk>
2019-05-21 13:18:16 +0200

    Made Accessory class Made AccessoryManager Class Made methods in Controller Changes in from void to string methods in Contoller

---
+++ DelpinCore/Accessory.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelpinCore
+{
+    class Accessory
+    {
+        public int accessoryModelID { get; private set; }
+
+        public string modelID { get; private set; }
+
+        public string accessoryID { get; private set; }
+
+        public Accessory(int accessoryModelID,string modelID, string accessoryID)
+        {
+            this.accessoryModelID = accessoryModelID;
+            this.modelID = modelID;
+            this.accessoryID = accessoryID;
+        }
+
+
+    }
+}
