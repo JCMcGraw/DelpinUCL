eefc0ecc41133d07f2a4c32752d97488a648e311
JCMcGraw <49905256+JCMcGraw@users.noreply.github.com>
2019-05-29 09:32:33 +0200

    Added Branch+BranchManager

---
+++ DelpinCore/BranchManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace DelpinCore
+{
+    class BranchManager
+    {
+        public DataTable ReadAllBranches()
+        {
+            string readAllBranchesSQL = "select * from Branch";
+
+            DataTable dataTable = DatabaseManager.ReadFromDatabase(readAllBranchesSQL);
+            return dataTable;
+        }
+
+        public Branch ReadBranchByBranchID(int branchID)
+        {
+            string readBranchByBranchIDSQL = $"select * from branch where BranchID = {branchID}";
+
+            DataTable dataTable = DatabaseManager.ReadFromDatabase(readBranchByBranchIDSQL);
+
+            string street = (string)dataTable.Rows[0]["Street"];
+            int postCode = (int)dataTable.Rows[0]["Postalcode"];
+            string city = (string)dataTable.Rows[0]["City"];
+            string phone = (string)dataTable.Rows[0]["Phone"];
+            string email = (string)dataTable.Rows[0]["Email"];
+
+            Branch branch = new Branch(branchID, street, postCode, city, phone, email);
+
+            return branch;
+        }
+    }
+}
